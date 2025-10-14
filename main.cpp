06301b52d250ef5eccccc427b9cbafbbac661018
Peter-Kisselev <peter.kisselev@gmail.com>
2025-10-14 14:38:40 -0400

    Hello world

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <iostream>
+#include <tuple>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <numeric>
+#include <sstream>
+#include <time.h>
+#include <stdlib.h>
+#include <random>
+#include <filesystem>
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/dnn/dnn.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[]) {
+	cout << "Hello world" << endl;
+	return 0;
+}
