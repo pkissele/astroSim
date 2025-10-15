f1577038afe36dbc62ff6df1ce171f7b20bb429d
Peter-Kisselev <peter.kisselev@gmail.com>
2025-10-15 13:07:26 -0400

    autogenerate visualization

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,9 @@
 #include <stdlib.h>
 #include <random>
 #include <filesystem>
+#include <cstdlib>
 
-#include <iostream>
-#include <vector>
+
 #include <Eigen/Dense>
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_odeiv2.h>
@@ -58,37 +58,43 @@ void draw_frame(const std::vector<Body>& bodies, const std::string& filename) {
     }
 
     cairo_surface_write_to_png(surface, filename.c_str());
-	cout << filename << endl;
     cairo_destroy(cr);
     cairo_surface_destroy(surface);
 }
 
 
 int main() {
-	const int N = 1;
+	const int N = 2;
 	vector<Body> bodies(N);
 
-	bodies[0] = Body{1, Vec2(-20, 0), Vec2(10, 0)};
+	bodies[0] = Body{1, Vec2(-5, 0), Vec2(1, 0)};
+    bodies[1] = Body{1, Vec2(5, 0), Vec2(-1, 0)};
 
 
 	double t=0;
-    double dt=0.05;
-    int nframes = 5;
+    int endT = 5;
+    double dt=1;
+    int nframes = (endT-t)/dt;
 
-    for (int frame=0; frame<nframes; ++frame) {
+    draw_frame(bodies, "output/frame_" + std::to_string(0) + ".png");
+
+    for (int frame=1; frame<nframes; ++frame) {
         double ti = t + dt;
 		cout << frame << endl;
 
         // update particles positions
         for (int i=0; i<N; ++i) {
-            bodies[i].pos[0] += bodies[i].vel[0];
-            bodies[i].pos[1] += bodies[i].vel[1];
+            bodies[i].pos[0] += bodies[i].vel[0]*dt;
+            bodies[i].pos[1] += bodies[i].vel[1]*dt;
 		}
 
         // save frame
         draw_frame(bodies, "output/frame_" + std::to_string(frame) + ".png");
     }
 
+    string pyComm = "python img2gif.py output " + to_string(nframes) + " " + to_string(dt*1000);
+
+    system(pyComm.c_str());
 
 	return 0;
 }
